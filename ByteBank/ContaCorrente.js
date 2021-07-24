8d75528e178ffa39933a6fea40e1ea00b8a0c8c9
andbarranha <84405280+andbarranha@users.noreply.github.com>
2021-07-23 23:06:56 -0300

    correcao texa e mostrar agencia

--- ByteBank/ContaCorrente.js
+++ ByteBank/ContaCorrente.js
@@ -1,6 +1,3 @@
-import {
-    Cliente
-} from "./Cliente.js";
 import { Conta } from "./Conta.js";
 
 export class ContaCorrente extends Conta{
@@ -8,5 +5,11 @@ export class ContaCorrente extends Conta{
     constructor(cliente, agencia) {
         super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
+    }
+
+    sacar(valor) {
+        let taxa = 1.1;
+        return this._sacar(valor,taxa);
+        
     }
 }
