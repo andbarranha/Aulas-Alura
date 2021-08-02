2f14b75ee5d745f7e00e4528b544cb9f4b23879a
andbarranha <anderson.barranha@gmail.com>
2021-08-01 23:38:27 -0300

    sistema interno
    sistema inter

--- ByteBank/index.js
+++ ByteBank/index.js
@@ -1,15 +1,16 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import {Gerente} from "./Funcionarios/Gerente.js";
+import {Diretor} from "./Funcionarios/Diretor.js";
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js";
 
+const diretor = new Diretor ("Rodrigo", 1000, 12345678900);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente ("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha("123")
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente = new Cliente("Lais", 789456612379, "456");
+const gerenteEstalogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstalogado = SistemaAutenticacao.login(diretor, "123456");
+console.log(gerenteEstalogado, diretorEstalogado);
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
-
-console.log(contaSalario);
+const clienteEstalogado = SistemaAutenticacao.login(cliente, "456");
