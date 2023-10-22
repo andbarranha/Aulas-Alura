64e33b33131138ff23646863e5ed6b5c3ee63f7f
andbarranha <84405280+andbarranha@users.noreply.github.com>
2023-10-22 13:41:58 -0300

    Comparando Variáveis

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,7 @@
 alert("Boas Vindas ao jogo do número secreto");
-let numeroSecreto = 29;
+let numeroSecreto = 5;
 let chute = prompt("Escolha um número entre 1 e 30");
 
-if(numeroSecreto == chute) {
-    console.log ("Isso aí! Voce descobriu o número sercreto");
+if (chute == numeroSecreto) {
+    console.log ("Isso aí! Voce descobriu o número sercreto (5)");
 }
