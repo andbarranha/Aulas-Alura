6d69ee608dfa41b95857b895321f303ae0e4868c
andbarranha <84405280+andbarranha@users.noreply.github.com>
2023-10-26 15:13:22 -0300

    Novas condições

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,15 @@
 alert("Boas Vindas ao jogo do número secreto");
 let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 e 30");
+console.log(numeroSecreto)
+let chute = prompt("Escolha um número entre 1 e 10");
 
+// se chute for igual ao número secreto 
 if (chute == numeroSecreto) {
-    console.log ("Isso aí! Voce descobriu o número sercreto (5)");
-}
+   alert (`Isso aí! Voce descobriu o número sercreto ${numeroSecreto}`);
+ } else {
+   if(chute > numeroSecreto){
+      alert(`O Número secreto é menor que ${chute}`);
+   } else{
+      alert(`O Número secreto é maior que ${chute}`)
+   }
+ }
